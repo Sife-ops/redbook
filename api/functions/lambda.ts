fd6dc4e6cb08ab260e88bae546589b88af84a045
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2022-06-21 20:12:59 -0700

    install tweetnacl

--- api/functions/lambda.ts
+++ api/functions/lambda.ts
@@ -1,6 +1,9 @@
 import { APIGatewayProxyHandlerV2 } from "aws-lambda";
 
 export const handler: APIGatewayProxyHandlerV2 = async (event) => {
+  // console.log(event);
+  console.log(process.env.PUBLIC_KEY);
+
   return {
     statusCode: 200,
     headers: { "Content-Type": "text/plain" },
