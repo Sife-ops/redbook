43f828d8c3b0cd5920a7c4a7c2fdc01f949e011e
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2022-06-21 21:03:44 -0700

    test command

---
+++ command/foo.ts
@@ -0,0 +1,28 @@
+// console.log(process.env.TEST)
+// console.log('lol')
+// require('dotenv').config()
+// const axios = require("axios").default;
+
+import axios from "axios";
+
+const { APP_ID, GUILD_ID, BOT_TOKEN } = process.env;
+if (!APP_ID || !GUILD_ID || !BOT_TOKEN) {
+  throw new Error("variable not defined");
+}
+
+let url = `https://discord.com/api/v8/applications/${APP_ID}/guilds/${GUILD_ID}/commands`;
+
+const headers = {
+  Authorization: `Bot ${BOT_TOKEN}`,
+  "Content-Type": "application/json",
+};
+
+let command_data = {
+  name: "foo",
+  type: 1,
+  description: "replies with bar ;/",
+};
+
+axios.post(url, JSON.stringify(command_data), {
+  headers: headers,
+});
