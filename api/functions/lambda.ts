885d4ca75e132131e96e638b4c46c8ff8833f19f
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2022-06-21 20:35:56 -0700

    verified

--- api/functions/lambda.ts
+++ api/functions/lambda.ts
@@ -1,12 +1,33 @@
 import { APIGatewayProxyHandlerV2 } from "aws-lambda";
+import nacl from "tweetnacl";
 
 export const handler: APIGatewayProxyHandlerV2 = async (event) => {
-  // console.log(event);
-  console.log(process.env.PUBLIC_KEY);
+  const publicKey = process.env.PUBLIC_KEY;
+  const signature = event.headers["x-signature-ed25519"];
+  const timestamp = event.headers["x-signature-timestamp"];
+  const bodyStr = event.body;
 
-  return {
-    statusCode: 200,
-    headers: { "Content-Type": "text/plain" },
-    body: `Hello, World! Your request was received at ${event.requestContext.time}.`,
+  const responseBad = {
+    statusCode: 401,
+    body: event.body,
   };
+
+  if (!publicKey || !signature || !timestamp || !bodyStr) {
+    return responseBad;
+  }
+
+  const verified = nacl.sign.detached.verify(
+    Buffer.from(timestamp + bodyStr),
+    Buffer.from(signature, "hex"),
+    Buffer.from(publicKey, "hex")
+  );
+
+  if (!verified) {
+    return responseBad;
+  } else {
+    return {
+      statusCode: 200,
+      body: event.body,
+    };
+  }
 };
