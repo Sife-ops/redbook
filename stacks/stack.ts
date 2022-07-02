13ec472a48c3177a0a806860a6b6761b55771837
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2022-07-02 04:13:34 -0700

    write data to bucket

--- stacks/stack.ts
+++ stacks/stack.ts
@@ -1,25 +1,26 @@
-import { StackContext, Api } from '@serverless-stack/resources';
+import {
+  Api,
+  Bucket,
+  Function,
+  StackContext,
+} from '@serverless-stack/resources';
 
-const {
-  POSTGRES_DATABASE,
-  POSTGRES_HOST,
-  POSTGRES_PASSWORD,
-  POSTGRES_PORT,
-  POSTGRES_USERNAME,
-  PUBLIC_KEY,
-  REDBOOK_ENV,
-} = process.env;
+const { PUBLIC_KEY, REDBOOK_ENV } = process.env;
+
+const DB = {
+  POSTGRES_DATABASE: process.env.POSTGRES_DATABASE,
+  POSTGRES_HOST: process.env.POSTGRES_HOST,
+  POSTGRES_PASSWORD: process.env.POSTGRES_PASSWORD,
+  POSTGRES_PORT: process.env.POSTGRES_PORT,
+  POSTGRES_USERNAME: process.env.POSTGRES_USERNAME,
+};
 
 export function stack({ stack }: StackContext) {
   const api = new Api(stack, 'api', {
     defaults: {
       function: {
         environment: {
-          POSTGRES_DATABASE,
-          POSTGRES_HOST,
-          POSTGRES_PASSWORD,
-          POSTGRES_PORT,
-          POSTGRES_USERNAME,
+          ...DB,
           PUBLIC_KEY,
           REDBOOK_ENV,
         },
@@ -27,11 +28,24 @@ export function stack({ stack }: StackContext) {
     },
 
     routes: {
-      'POST /': 'functions/lambda.handler',
+      'POST /': 'functions/bot.handler',
     },
   });
 
+  const exportJsonBucket = new Bucket(stack, 'exportJsonBucket');
+
+  const exportJsonLambda = new Function(stack, 'exportJsonLambda', {
+    handler: 'functions/export-json.handler',
+    environment: {
+      ...DB,
+      BUCKET: exportJsonBucket.bucketName,
+    },
+    permissions: [exportJsonBucket],
+  });
+
   stack.addOutputs({
+    // todo: rename
     ApiEndpoint: api.url,
+    ExportJsonBucket: exportJsonBucket.bucketName,
   });
 }
