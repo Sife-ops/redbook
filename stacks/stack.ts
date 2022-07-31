093652bbdc12eb989add748873ea8f4f9127949d
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2022-07-31 03:31:31 -0700

    dynamodb table

--- stacks/stack.ts
+++ stacks/stack.ts
@@ -3,6 +3,7 @@ import {
   Bucket,
   Function,
   StackContext,
+  Table,
 } from '@serverless-stack/resources';
 
 const { PUBLIC_KEY, REDBOOK_ENV } = process.env;
@@ -16,13 +17,46 @@ const DB = {
 };
 
 export function stack({ stack }: StackContext) {
+  const table = new Table(stack, "table", {
+    fields: {
+      pk: "string",
+      sk: "string",
+      gsi1pk: "string",
+      gsi1sk: "string",
+      gsi2pk: "string",
+      gsi2sk: "string",
+      gsi3pk: "string",
+      gsi3sk: "string",
+    },
+    primaryIndex: {
+      partitionKey: "pk",
+      sortKey: "sk",
+    },
+    globalIndexes: {
+      gsi1: {
+        partitionKey: "gsi1pk",
+        sortKey: "gsi1sk",
+      },
+      gsi2: {
+        partitionKey: "gsi2pk",
+        sortKey: "gsi2sk",
+      },
+      gsi3: {
+        partitionKey: "gsi3pk",
+        sortKey: "gsi3sk",
+      },
+    },
+  });
+
   const bot = new Api(stack, 'api', {
     defaults: {
       function: {
+        permissions: [table],
         environment: {
           ...DB,
           PUBLIC_KEY,
           REDBOOK_ENV,
+          TABLE: table.tableName
         },
       },
     },
