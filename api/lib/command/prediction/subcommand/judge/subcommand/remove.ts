aa69f1eab236269fd4c974f0ac4b608e3f760ee9
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2022-08-01 19:08:52 -0700

    prediction judge remove

--- api/lib/command/prediction/subcommand/judge/subcommand/remove.ts
+++ api/lib/command/prediction/subcommand/judge/subcommand/remove.ts
@@ -1,6 +1,6 @@
 import Joi from 'joi';
-import { db } from '@redbook/lib/model';
 import { optionValue } from '@redbook/lib/utility';
+import { redbookModel } from '@redbook/lib/db';
 
 export const remove = {
   schema: Joi.object({
@@ -33,93 +33,99 @@ export const remove = {
   }).options({ allowUnknown: true }),
 
   handler: async (body: any) => {
-    const predictionUserId = body.member.user.id;
-
+    const prognosticatorId = body.member.user.id;
     const { options } = body.data.options[0].options[0];
     const predictionId = optionValue(options, 'id');
 
-    try {
-      const prediction = await db
-        .selectFrom('prediction')
-        .where('user_id', '=', predictionUserId)
-        .where('id', '=', predictionId)
-        .selectAll()
-        .executeTakeFirstOrThrow();
+    const { PredictionEntity, JudgeEntity } = await redbookModel.collections.predictionJudges({
+      predictionId,
+    }).go()
 
-      const judgeUserId = optionValue(options, 'judge');
-
-      const judges = await db
-        .selectFrom('judge')
-        .where('prediction_id', '=', prediction.id)
-        .selectAll()
-        .execute();
-
-      if (judges.length < 2) {
-        return {
-          type: 4,
-          data: {
-            content: `<@${predictionUserId}> Removing judge failed because there must be at least one judge.`,
-          },
-        };
-      }
-
-      // must remove self if you are a judge and there are only 2 judges
-      if (
-        judges.length < 3 &&
-        judges.find((e) => e.id === predictionUserId) &&
-        judgeUserId !== predictionUserId
-      ) {
-        return {
-          type: 4,
-          data: {
-            content: `<@${predictionUserId}> Removing judge failed because that would make you the default judge. Remove yourself first.`,
-          },
-        };
-      }
-
-      await db
-        .deleteFrom('judge')
-        .where('user_id', '=', judgeUserId)
-        .where('prediction_id', '=', predictionId)
-        .returningAll()
-        .executeTakeFirstOrThrow();
-
+    // prediction must exist and belong to user
+    if (
+      PredictionEntity.length < 1 ||
+      PredictionEntity[0].prognosticatorId !== prognosticatorId
+    ) {
       return {
         type: 4,
         data: {
-          embeds: [
-            {
-              title: 'Judge Removed',
-              description: prediction.conditions,
-              color: 0x00baff,
-              fields: [
-                {
-                  name: 'By',
-                  value: `<@${predictionUserId}>`,
-                  inline: true,
-                },
-                {
-                  name: 'Judge',
-                  value: `<@${judgeUserId}>`,
-                  inline: true,
-                },
-                {
-                  name: 'ID',
-                  value: predictionId,
-                  inline: false,
-                },
-              ],
-            },
-          ],
-        },
-      };
-    } catch {
-      return {
-        type: 4,
-        data: {
-          content: `<@${predictionUserId}> Removing judge failed because the prediction does not exist or it is not your prediction.`,
+          content: `<@${prognosticatorId}> Removing judge failed because the prediction does not exist or it is not your prediction.`,
         },
       };
     }
+
+    const judgeId = optionValue(options, 'judge');
+
+    // specified user must be a judge
+    if (!JudgeEntity.find(e => e.judgeId === judgeId)) {
+      return {
+        type: 4,
+        data: {
+          content: `<@${prognosticatorId}> Removing judge failed because the specified user is not a judge.`,
+        },
+      };
+    }
+
+    // cannot remove last judge
+    if (JudgeEntity.length < 2) {
+      return {
+        type: 4,
+        data: {
+          content: `<@${prognosticatorId}> Removing judge failed because there must be at least one judge.`,
+        },
+      };
+    }
+
+    // must remove self if you are a judge and there are only 2 judges
+    if (
+      JudgeEntity.length < 3 &&
+      JudgeEntity.find((e) => e.judgeId === prognosticatorId) &&
+      judgeId !== prognosticatorId
+    ) {
+      return {
+        type: 4,
+        data: {
+          content: `<@${prognosticatorId}> Removing judge failed because that would make you the default judge. Remove yourself first.`,
+        },
+      };
+    }
+
+    // todo: use try/catch
+    redbookModel.entities.JudgeEntity.remove({
+      judgeId,
+      predictionId,
+    }).go()
+
+    const prediction = PredictionEntity[0]
+
+    return {
+      type: 4,
+      data: {
+        embeds: [
+          {
+            title: 'Judge Removed',
+            description: prediction.conditions,
+            color: 0x00baff,
+            fields: [
+              {
+                name: 'By',
+                value: `<@${prognosticatorId}>`,
+                inline: true,
+              },
+              {
+                name: 'Judge',
+                value: `<@${judgeId}>`,
+                inline: true,
+              },
+              {
+                name: 'ID',
+                value: predictionId,
+                inline: false,
+              },
+            ],
+          },
+        ],
+      },
+    };
   },
 };
