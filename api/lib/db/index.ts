0fcad81ef52c107b9042884ff1967b5a8cbb6d91
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2022-08-02 00:14:19 -0700

    remove prognosticator entity

--- api/lib/db/index.ts
+++ api/lib/db/index.ts
@@ -2,11 +2,9 @@ import { Service } from "electrodb";
 
 import { JudgeEntity } from './judge'
 import { PredictionEntity } from './prediction'
-import { PrognosticatorEntity } from './prognosticator'
 
 export const redbookModel = new Service({
   JudgeEntity,
   PredictionEntity,
-  PrognosticatorEntity
 })
 
