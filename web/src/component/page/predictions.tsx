d805a218bbd48ec2dc91ceddc72b659f065e50e4
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2022-08-27 15:40:06 -0700

    background css

--- web/src/component/page/predictions.tsx
+++ web/src/component/page/predictions.tsx
@@ -21,31 +21,29 @@ export const Predictions: React.FC = () => {
   const { predictions } = predictionsQueryState.data;
 
   if (predictions.length < 1) {
-    return <h2 tw="text-center">no predictos</h2>;
+    return <h2 tw="text-center">No predictos!</h2>;
   }
 
   return (
     <div>
       <h2 tw="text-center">{predictions[0].user.username}'s Predictos:</h2>
-      <div>
-        {predictions.map(e => (
-          <div
-            key={e.predictionId}
-            css={[
-              tw`mb-2 p-2 rounded-md bg-gray-700`,
-              () => {
-                if (e.verdict === 'correct') {
-                  return tw`bg-green-900 border-2 border-solid border-green-500`;
-                } else if (e.verdict === 'incorrect') {
-                  return tw`bg-red-900 border-2 border-solid border-red-500`;
-                }
+      {predictions.map(e => (
+        <div
+          key={e.predictionId}
+          css={[
+            tw`mb-2 p-2 rounded-md bg-gray-700`,
+            () => {
+              if (e.verdict === 'correct') {
+                return tw`bg-green-900 border-2 border-solid border-green-500`;
+              } else if (e.verdict === 'incorrect') {
+                return tw`bg-red-900 border-2 border-solid border-red-500`;
               }
-            ]}
-          >
-            <Prediction prediction={e} type="summary" />
-          </div>
-        ))}
-      </div>
+            }
+          ]}
+        >
+          <Prediction prediction={e} type="summary" />
+        </div>
+      ))}
     </div>
   );
 };
--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,3 +1,4 @@
+import './index.css'
 import 'twin.macro';
 import GlobalStyles from './styles/global-styles';
 import ReactDOM from 'react-dom/client';
