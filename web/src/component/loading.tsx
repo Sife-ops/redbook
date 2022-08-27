a374e1b9ed0d7348bb86e8f7548d6e2e5d9de2ea
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2022-08-27 16:45:25 -0700

    remove overflow hidden

--- web/src/component/loading.tsx
+++ web/src/component/loading.tsx
@@ -2,9 +2,10 @@ import 'twin.macro';
 import React from 'react';
 import spinner from '../assets/spinner.gif';
 
-export const Loading: React.FC = props => {
+// todo: can't get rid of scroll
+export const Loading: React.FC = () => {
   return (
-    <div tw="flex flex-col justify-center h-full">
+    <div tw="flex flex-col justify-center h-[200px]">
       <div tw="flex justify-center">
         <img src={spinner} tw="w-16" />
       </div>
